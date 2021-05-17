a5bd804f96b1e4d608cdf570a5ca188af899aaa9
Manuel Filgueira Marin <manuelfilgueira@gmail.com>
2021-05-17 21:36:17 +0000

    proyectoactualizado

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 //import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "Prop-Types";
+import PropTypes from "prop-types";
 //include bootstrap npm library into the bundle
 import "bootstrap";
 
@@ -9,33 +9,43 @@ import "bootstrap";
 import "../styles/index.scss";
 
 function SimpleCounter(props) {
-    return (<div className "bigCounter" >
-        <div className "calendario"><i className="far fa-clock"></i></div >
-        <div className "cuatro">{props.digitCuatro}</div>
-        <div className "tres">{props.digitTres}</div>
-        <div className "dos">{props.digiDos}</div>
-        <div className "uno">{props.digitUno}</div>
-    </div>);
+	return (
+		<div className="bigCounter">
+			<div className="calendario">
+				<i className="far fa-clock"></i>
+			</div>
+			<div className="cuatro">{props.digiCuatro}</div>
+			<div className="tres">{props.digiTres}</div>
+			<div className="dos">{props.digiDos}</div>
+			<div className="uno">{props.digiUno}</div>
+		</div>
+	);
 }
-SimpleCounter.PropTypes = {
-    digitCuatro: PropTypes.number,
-    digitTres: PropTypes.number,
-    digitDos: PropTypes.number,
-    digitUno: PropTypes.number,
+SimpleCounter.propTypes = {
+	digiCuatro: PropTypes.number,
+	digiTres: PropTypes.number,
+	digiDos: PropTypes.number,
+	digiUno: PropTypes.number
 };
 
 let counter = 0;
-setInterval function(){
-    const cuatro = Math.floor (counter/10000); 
-    const tres = Math.floor (counter/1000);
-    const dos = Math.floor (counter/100); 
-    const uno = Math.floor (counter/10); 
-    console.log (cuatro, tres, dos,uno);
+setInterval(function() {
+	const cuatro = Math.floor(counter / 1000);
+	const tres = Math.floor(counter / 100);
+	const dos = Math.floor(counter / 10);
+	const uno = Math.floor(counter / 1);
+	console.log(cuatro, tres, dos, uno);
 
-//render your react application
-counter++;
+	//render your react application
+	counter++;
 
-ReactDOM.render(
-<simplecounter digitUno={uno} digitDos={dos} digitTres={tres} digitCuatro={cuatro} />, 
-document.querySelector('#app')
-;} 1000);
+	ReactDOM.render(
+		<SimpleCounter
+			digiUno={uno}
+			digiDos={dos}
+			digiTres={tres}
+			digiCuatro={cuatro}
+		/>,
+		document.querySelector("#app")
+	);
+}, 1000);
